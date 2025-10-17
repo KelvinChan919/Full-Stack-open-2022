b9d69a07b2f15d8b28b8eba47e58d66119fa8c05
Kelvin Chan <kelvin.chan2000919@gmail.com>
2025-10-17 16:12:46 +0800

    finished anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -17,11 +17,17 @@ const App = () => {
   const [vote, setvote] = useState(vote_array)
   const [show_vote, set_show_vote] = useState(0)
   const [selected, setSelected] = useState(0)
+  const [biggest_index, set_biggest_index] = useState(0)
+  const [biggest_vote, set_biggest_vote] = useState(0)
   const vote_func = () => {
     vote[selected] += 1
     const new_vote = vote
     setvote(new_vote)
     set_show_vote(vote[selected])
+    const max_num = Math.max(...new_vote)
+    const max_index = new_vote.indexOf(max_num)
+    set_biggest_index(max_index)
+    set_biggest_vote(max_num)
   }
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -33,10 +39,14 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <div>has {vote[selected]} votes</div>
       <div><button onClick={vote_func}>vote</button></div>
       <div><button onClick={gen_random_num}>next anecdote</button></div>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[biggest_index]}
+      <div>has {biggest_vote} votes</div>
     </div>
   )
 }
