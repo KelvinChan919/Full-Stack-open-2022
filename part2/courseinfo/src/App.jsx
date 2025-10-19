f9b6ca0337eb18e0836e4339bcc016e9b65ead7b
kelvinchan919 <kelvin.chan2000919@gmail.com>
2025-10-19 16:36:20 +0800

    finished exercise 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,80 @@
+const Courses = (courses) => {
+  return(
+    <div>
+      <h1>{courses.name}</h1>
+      <ul>
+        {courses.each}
+      </ul>
+      <p>total of {courses.total} exercises</p>
+    </div>
+  )
+}
+
 const Course = (course) => {
     return(
       <li>{course.name} {course.exercises}</li>
     )
 }
 
+
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-  const total = (course.parts.map(element => element.exercises)).reduce((accumulator, currentvalue) => accumulator + currentvalue)
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return(
     <div>
-      <h1>{course.name}</h1>
-      <ul>  
-        {course.parts.map(course =>
-          <Course key={course.id} name={course.name} exercises={course.exercises}/>
-        )}
-      </ul>
-      <p>total of {total} exercises</p>
+      {courses.map(element =>{
+        const course_name = element.name
+        const course_id = element.id
+        const course_parts = element.parts
+        const each_total = course_parts.map(element => element.exercises).reduce((accumulator, currentvalue) => accumulator + currentvalue)
+        const parts_components = course_parts.map(component => <Course name={component.name} exercises={component.exercises} key={component.id}/>)
+        return(
+          <Courses name={course_name} key={course_id} each={parts_components} total={each_total}/>
+        )
+      })}
     </div>
   )
 }
