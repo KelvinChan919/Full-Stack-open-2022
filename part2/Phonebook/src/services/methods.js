7bce15323aba38a45dd6d1cd354a00a84c66064b
kelvinchan919 <kelvin.chan2000919@gmail.com>
2025-10-26 16:28:51 +0800

    finished PhoneBook step 8

--- part2/Phonebook/src/services/methods.js
+++ part2/Phonebook/src/services/methods.js
@@ -3,17 +3,17 @@ const baseurl = 'http://localhost:3001/persons'
 
 const getall = ()=>{
     const request = axios.get(baseurl)
-    return request
+    return request.then(response => {return response})
 }
 
 const create =(newObject) => {
     const request = axios.post(baseurl,newObject)
-    return request
+    return request.then(response => {return response})
 }
 
 const update = (id,newObject) => {
     const request = axios.put(`${baseurl}/${id}`, newObject)
-    return request
+    return request.then(response => {return response}) 
 }
 
 export default { getall, create, update }
