9dc6e16ca9ebe25052c801deba2c70afea71758f
Kelvin Chan <kelvin.chan2000919@gmail.com>
2025-11-05 12:34:16 +0800

    finished 4.4 helper function step 2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blog) => {
+    const initial = 0
+    return blog.map(each => each.likes).reduce((accumulator, current) => accumulator + current, initial)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
