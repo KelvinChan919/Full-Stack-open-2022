1bf2339e6bf3157c71695db4d5b2c0b350711603
Kelvin Chan <kelvin.chan2000919@gmail.com>
2025-11-05 15:22:02 +0800

    finished 4.7 helper function step 5 and all helper function exercises

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -23,9 +23,22 @@ const mostBlogs = (blog) => {
     return arr[arr.map( entry => entry.blogs).indexOf(Math.max(...arr.map(entry => entry.blogs)))]
 }
 
+const mostLikes = (blog) => {
+    const arr = []
+    const author_list = [...new Set(blog.map(entry => entry.author))]
+    for(i=0; i < author_list.length; i++){
+        const total_likes = blog.filter(entry => entry.author === author_list[i]).map(entry => entry.likes).reduce((accumulator, current) => accumulator + current, 0)
+        console.log(total_likes)
+        const entry = { author : author_list[i], likes : total_likes }
+        arr.push(entry)
+    }
+    return arr[arr.map( entry => entry.likes).indexOf(Math.max(...arr.map(entry => entry.likes)))]
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
