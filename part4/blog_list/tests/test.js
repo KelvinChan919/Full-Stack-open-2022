931066996853abddbfbb71aea370fac5f9eff638
Kelvin Chan <kelvin.chan2000919@gmail.com>
2025-11-05 15:00:40 +0800

    finished 4.5 and 4.6 helper function step 3 and 4

--- part4/blog_list/tests/test.js
+++ part4/blog_list/tests/test.js
@@ -66,21 +66,9 @@ const blogs = [
     assert.strictEqual(result, 36)
   })
 
-  test('the favorite blog', () => {
+    test('the favorite blog', () => {
     const result = listHelper.favoriteBlog(blogs)
-    assert.deepStrictEqual(result,   {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  })
-  })
-
-  test('the author with most blogs', () => {
-    const result = listHelper.mostBlogs(blogs)
-    assert.deepStrictEqual(result,{ author: "Robert C. Martin", blogs: 3 })
+    assert.deepStrictEqual(result, { author: "Robert C. Martin", blogs: 3 })
   })
 })
 
